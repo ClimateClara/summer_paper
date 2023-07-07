1a1a232710852b791c8c16ce9396f040e3c85b0e
Clara Burgard <clara.burgard@gmail.com>
2023-07-07 19:19:14 +0200

    advanced on Christoph data but still missing data over the continent

--- notebooks/Ocean_stuff/new_tuning/format_Christoph_data.sh
+++ notebooks/Ocean_stuff/new_tuning/format_Christoph_data.sh
@@ -34,22 +34,25 @@ echo $yy
 cdo merge $path2/fwfisf-eANT025.L121-"$nemo_run"_"$yy".nc $path2/thetao-eANT025.L121-"$nemo_run"_"$yy".nc $path2/so-eANT025.L121-"$nemo_run"_"$yy".nc $path2/variables_of_interest-"$nemo_run"_"$yy".nc
 done
 
-cdo setgrid,$path2/variables_of_interest-"$nemo_run"_"$yy".nc $path1/domain_cfg_eANT025.L121.nc $path2/domain_cfg_eANT025.L121_setgrid.nc
+cdo setgrid,$path2/variables_of_interest-"$nemo_run"_2015.nc $path1/domain_cfg_eANT025.L121.nc $path2/domain_cfg_eANT025.L121_setgrid.nc
+#$path1/eANT025.L121_grid_T_cdo.nc
 
 for yy in {2014..2100}
 do 
 echo $yy
 cdo sellonlatbox,0,360,-90,-50 $path2/variables_of_interest-"$nemo_run"_"$yy".nc $path3/variables_of_interest_"$yy"_Ant.nc
 done
-cdo sellonlatbox,0,360,-90,-50 $path2/domain_cfg_eANT025.L121_setgrid.nc $path3/mask_variables_of_interest_Ant.nc
+cdo sellonlatbox,0,360,-90,-50 -selgrid,1 $path2/domain_cfg_eANT025.L121_setgrid.nc $path3/mask_variables_of_interest_Ant.nc
 
-
-cdo ifthenc,2 -subc,1 -selvar,tmask $path3/mask_variables_of_interest_Ant.nc $path3/lsmask_0-2_Ant_withmiss.nc  
+cdo gtc,0 -selvar,so $path3/variables_of_interest_2015_Ant.nc $path2/mask_ocean.nc
+cdo subc,1 $path2/mask_ocean.nc $path3/lsmask_-1-0_Ant_withmiss.nc
+cdo ifthenc,2 $path3/lsmask_-1-0_Ant_withmiss.nc $path3/lsmask_0-2_Ant_withmiss.nc  
 cdo setmisstoc,0 $path3/lsmask_0-2_Ant_withmiss.nc $path3/lsmask_0-2_Ant.nc 
 
 cdo ifthenc,1 -selvar,isf_draft $path3/mask_variables_of_interest_Ant.nc $path3/isfmask_1_Ant_withmiss.nc 
 cdo setmisstoc,0 $path3/isfmask_1_Ant_withmiss.nc $path3/isfmask_1_Ant.nc 
 
 cdo add $path3/lsmask_0-2_Ant.nc $path3/isfmask_1_Ant.nc $path3/lsmask_0-1-2_andsome3s_Ant.nc
-cdo mul -lec,2 $path3/lsmask_0-1-2_andsome3s_Ant.nc $path3/lsmask_0-1-2_andsome3s_Ant.nc $path3/lsmask_0-1-2_Ant_nonfloat.nc
+cdo lec,2 $path3/lsmask_0-1-2_andsome3s_Ant.nc $path3/lsmask_below2s_Ant.nc
+cdo mul $path3/lsmask_below2s_Ant.nc $path3/lsmask_0-1-2_andsome3s_Ant.nc $path3/lsmask_0-1-2_Ant_nonfloat.nc
 cdo mulc,1.0 $path3/lsmask_0-1-2_Ant_nonfloat.nc $path3/lsmask_0-1-2_Ant.nc 
