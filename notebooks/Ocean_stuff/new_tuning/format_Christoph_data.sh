df11a937058476e0f500d68f1f874a0593b0d2a8
Clara Burgard <clara.burgard@gmail.com>
2023-07-14 16:47:49 +0200

    worked on correcting the white line in the stereo interpolation of Christoph data, took quite some effort but I think I got it !

--- notebooks/Ocean_stuff/new_tuning/format_Christoph_data.sh
+++ notebooks/Ocean_stuff/new_tuning/format_Christoph_data.sh
@@ -8,9 +8,9 @@
 homepath=/bettik/burgardc
 
 #### name of NEMO run
-#nemo_run=ctrl94
+nemo_run=ctrl94
 #nemo_run=isf94
-nemo_run=isfru94
+#nemo_run=isfru94
 
 ################# DECLARE THE PATHS ##############################
 path1=$homepath/DATA/SUMMER_PAPER/raw/CHRISTOPH_DATA
@@ -19,31 +19,69 @@ path3=$homepath/DATA/SUMMER_PAPER/interim/NEMO_"$nemo_run"_ANT_STEREO
 path5=$homepath/DATA/SUMMER_PAPER/raw
 ###################################################################
 
-#cdo splityear $path1/fwfisf-eANT025.L121-"$nemo_run"-1y_1982-2013.nc $path2/fwfisf-eANT025.L121-"$nemo_run"_
-#cdo splityear $path1/thetao-eANT025.L121-"$nemo_run"-1y_1982-2013.nc $path2/thetao-eANT025.L121-"$nemo_run"_
-#cdo splityear $path1/so-eANT025.L121-"$nemo_run"-1y_1982-2013.nc $path2/so-eANT025.L121-"$nemo_run"_
+cdo setgrid,$path1/var_grid_wo_nans.txt $path1/fwfisf-eANT025.L121-"$nemo_run"-1y_1982-2013.nc $path2/fwfisf-eANT025.L121-"$nemo_run"-1y_setgrid.nc
+cdo setgrid,$path1/var_grid_wo_nans.txt $path1/thetao-eANT025.L121-"$nemo_run"-1y_1982-2013.nc $path2/thetao-eANT025.L121-"$nemo_run"-1y_setgrid.nc
+cdo setgrid,$path1/var_grid_wo_nans.txt $path1/so-eANT025.L121-"$nemo_run"-1y_1982-2013.nc $path2/so-eANT025.L121-"$nemo_run"-1y_setgrid.nc
 
-cdo splityear $path1/fwfisf-eANT025.L121-"$nemo_run"-1y_2014-2100.nc $path2/fwfisf-eANT025.L121-"$nemo_run"_
-cdo splityear $path1/thetao-eANT025.L121-"$nemo_run"-1y_2014-2100.nc $path2/thetao-eANT025.L121-"$nemo_run"_
-cdo splityear $path1/so-eANT025.L121-"$nemo_run"-1y_2014-2100.nc $path2/so-eANT025.L121-"$nemo_run"_
+cdo splityear $path2/fwfisf-eANT025.L121-"$nemo_run"-1y_setgrid.nc $path2/fwfisf-eANT025.L121-"$nemo_run"_
+cdo splityear $path2/thetao-eANT025.L121-"$nemo_run"-1y_setgrid.nc $path2/thetao-eANT025.L121-"$nemo_run"_
+cdo splityear $path2/so-eANT025.L121-"$nemo_run"-1y_setgrid.nc $path2/so-eANT025.L121-"$nemo_run"_
 
-#for yy in {1982..2013}
-for yy in {2014..2100}
+#cdo splityear $path1/fwfisf-eANT025.L121-"$nemo_run"-1y_2014-2100.nc $path2/fwfisf-eANT025.L121-"$nemo_run"_
+#cdo splityear $path1/thetao-eANT025.L121-"$nemo_run"-1y_2014-2100.nc $path2/thetao-eANT025.L121-"$nemo_run"_
+#cdo splityear $path1/so-eANT025.L121-"$nemo_run"-1y_2014-2100.nc $path2/so-eANT025.L121-"$nemo_run"_
+
+for yy in {1982..2013}
+#for yy in {2014..2100}
 do 
 echo $yy
+\rm $path2/variables_of_interest-"$nemo_run"_"$yy".nc
 cdo merge $path2/fwfisf-eANT025.L121-"$nemo_run"_"$yy".nc $path2/thetao-eANT025.L121-"$nemo_run"_"$yy".nc $path2/so-eANT025.L121-"$nemo_run"_"$yy".nc $path2/variables_of_interest-"$nemo_run"_"$yy".nc
 done
 
-cdo setgrid,$path2/variables_of_interest-"$nemo_run"_2015.nc $path1/domain_cfg_eANT025.L121.nc $path2/domain_cfg_eANT025.L121_setgrid.nc
+
+cdo ifthenc,1 -vertsum -selvar,tmask $path1/mesh_mask.nc $path2/tmask_sum_withmiss.nc 
+cdo setmisstoc,0 $path2/tmask_sum_withmiss.nc $path2/tmask_sum.nc
+
+cdo ifthenc,1 -selvar,tmask $path1/mesh_mask.nc $path2/tmask_3d_withmiss.nc 
+cdo setmisstoc,0 $path2/tmask_3d_withmiss.nc $path2/tmask_3d.nc 
+
+for vvar in {nav_lon,nav_lat,isf_draft,bathy_metry,e3w_0}
+do
+    cdo selvar,$vvar $path1/domain_cfg_eANT025.L121.nc  $path2/mask_$vvar.nc
+done
+
+cdo merge $path2/mask_nav_lon.nc $path2/mask_nav_lat.nc $path2/tmask_sum.nc $path2/tmask_3d.nc $path2/mask_isf_draft.nc $path2/mask_bathy_metry.nc $path2/mask_e3w_0.nc  $path2/mask_variables_of_interest.nc
+# used python: mask_file.squeeze().drop('time_counter')  mfile_wo_time.to_netcdf('mask_variables_of_interest_wotime.nc')
+
+cdo setgrid,$path1/var_grid_wo_nans.txt $path2/mask_variables_of_interest_wotime.nc $path2/mask_variables_of_interest_setgrid.nc 
 #$path1/eANT025.L121_grid_T_cdo.nc
 
-for yy in {2014..2100}
+
+cdo sellonlatbox,0,360,-90,-50 $path2/mask_variables_of_interest_setgrid.nc $path3/mask_variables_of_interest_Ant.nc
+
+
+cdo ifthenc,2 -subc,1 -selvar,tmask $path3/mask_variables_of_interest_Ant.nc $path3/lsmask_0-2_Ant_withmiss.nc  
+cdo setmisstoc,0 $path3/lsmask_0-2_Ant_withmiss.nc $path3/lsmask_0-2_Ant.nc 
+
+cdo ifthenc,1 -selvar,isf_draft $path3/mask_variables_of_interest_Ant.nc $path3/isfmask_1_Ant_withmiss.nc 
+cdo setmisstoc,0 $path3/isfmask_1_Ant_withmiss.nc $path3/isfmask_1_Ant.nc 
+
+cdo add $path3/lsmask_0-2_Ant.nc $path3/isfmask_1_Ant.nc $path3/lsmask_0-1-2_andsome3s_Ant.nc
+cdo mul -lec,2 $path3/lsmask_0-1-2_andsome3s_Ant.nc $path3/lsmask_0-1-2_andsome3s_Ant.nc $path3/lsmask_0-1-2_Ant_nonfloat.nc
+cdo mulc,1.0 $path3/lsmask_0-1-2_Ant_nonfloat.nc $path3/lsmask_0-1-2_Ant.nc 
+
+#cdo gtc,0 -selvar,tmask_2 $path3/mask_variables_of_interest_Ant.nc $path3/lsmask_0-1_3d_Ant.nc
+
+for yy in {1982..2013}
+#for yy in {2014..2100}
 do 
 echo $yy
-cdo sellonlatbox,0,360,-90,-50 $path2/variables_of_interest-"$nemo_run"_"$yy".nc $path3/variables_of_interest_"$yy"_Ant.nc
+cdo  sellonlatbox,0,360,-90,-50 $path2/variables_of_interest-"$nemo_run"_"$yy".nc $path3/variables_of_interest_"$yy"_Ant_wolandmask.nc
 done
-cdo sellonlatbox,0,360,-90,-50 -selgrid,1 $path2/domain_cfg_eANT025.L121_setgrid.nc $path3/mask_variables_of_interest_Ant.nc
 
+################################ STOP HERE ###########
+#### USING SO TO DO THE MASK BUT LED TO A LINE NEAR AMERY
 cdo gtc,0 -selvar,bathy_metry $path3/mask_variables_of_interest_Ant.nc $path2/mask_ocean.nc
 cdo subc,1 $path2/mask_ocean.nc $path3/lsmask_-1-0_Ant_withmiss.nc
 cdo ifthenc,2 $path3/lsmask_-1-0_Ant_withmiss.nc $path3/lsmask_0-2_Ant_withmiss.nc  
