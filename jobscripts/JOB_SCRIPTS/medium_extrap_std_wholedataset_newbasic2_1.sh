f1b9bbd5a9f5cdd598fce91cab4c86f319e7f3a2
Clara Burgard <clara.burgard@gmail.com>
2024-01-12 17:07:47 +0100

    sent the retuning of the deep ensemble

---
+++ jobscripts/JOB_SCRIPTS/medium_extrap_std_wholedataset_newbasic2_1.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/summer_paper/notebooks/Ocean_stuff/new_tuning//run_training_whole_dataset_summer_paper.py medium extrap std newbasic2 1 2>&1 | tee /bettik/burgardc/SCRIPTS/summer_paper/jobscripts/JOB_OUTFILES//medium_extrap_std_wholedataset_newbasic2_1.log
---
+++ jobscripts/JOB_SCRIPTS/small_extrap_std_wholedataset_newbasic2_9.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/summer_paper/notebooks/Ocean_stuff/new_tuning//run_training_whole_dataset_summer_paper.py small extrap std newbasic2 9 2>&1 | tee /bettik/burgardc/SCRIPTS/summer_paper/jobscripts/JOB_OUTFILES//small_extrap_std_wholedataset_newbasic2_9.log
