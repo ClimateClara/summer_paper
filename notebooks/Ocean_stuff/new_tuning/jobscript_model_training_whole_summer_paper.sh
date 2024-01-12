6f40e1c41cfc652cbf945b7a42ccd7e948b28e5a
Clara Burgard <clara.burgard@gmail.com>
2024-01-12 17:07:01 +0100

    corrected the reference melt for isfru94 and retuned everything, missing plume because of correction

--- notebooks/Ocean_stuff/new_tuning/jobscript_model_training_whole_summer_paper.sh
+++ notebooks/Ocean_stuff/new_tuning/jobscript_model_training_whole_summer_paper.sh
@@ -6,7 +6,7 @@ mod_size=small  #'mini', 'small', 'medium', 'large', 'extra_large'
 TS_opt=extrap # extrap, whole, thermocline
 norm_method=std # std, interquart, minmax
 exp_name=newbasic2
-seed_nb=10
+seed_nb=1
 
 
 path_jobscripts=/bettik/burgardc/SCRIPTS/summer_paper/jobscripts/JOB_SCRIPTS/
@@ -25,7 +25,7 @@ EOF
 
 chmod +x $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.sh
 
-oarsub -S -n ${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb} --stdout $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.o%jobid%  --stderr $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.e%jobid% -l nodes=1/core=4,walltime=10:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.sh
+oarsub -S -n ${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb} --stdout $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.o%jobid%  --stderr $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.e%jobid% -l nodes=1/core=6,walltime=10:00:00 --project mais -p "network_address='luke60'" $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.sh
 
 # to remove if no CV!!!
 
